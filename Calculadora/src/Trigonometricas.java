1e70dbea2061d7b505049511978a9353a724f3dc
estudiantes <estudiantes@labing00>
2017-03-24 17:45:56 -0500

    4

---
+++ Calculadora/src/Trigonometricas.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Trigonometricas {
+  
+    import static java.lang.Math.cos;
+    import static java.lang.Math.sin;
+    import static java.lang.Math.tan;
+    
+    
+    double a;
+    double respuesta;
+    
+    Calculadora miCalculadora=new Calculadora(); 
+    public static double sin(double a) {
+        
+        return respuesta;
+    }
+    
+    
+  
+}
