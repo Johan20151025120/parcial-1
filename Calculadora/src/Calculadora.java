9d756690b568082bb389aeef333866bbe0936829
estudiantes <estudiantes@labing00>
2017-03-24 16:46:23 -0500

    1

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Calculadora {
+    
+  int Valor1;
+  int Valor2;
+  int resultado;
+    
+    
+  int suma () {
+      resultado = Valor1 + Valor2;
+      return resultado;        
+  }
+  
+  int resta () {
+      resultado = Valor1 + Valor2;
+      return resultado;        
+  }
+  
+  int multiplicacion () {
+      resultado = Valor1 + Valor2;
+      return resultado;        
+  }
+  
+  int division () {
+      resultado = Valor1 + Valor2;
+      return resultado;        
+  }
+  
+}
